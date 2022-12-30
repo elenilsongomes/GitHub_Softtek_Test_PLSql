1288842b5e33d1ebc873a150539e8ac4514425ab
elenilsongomes <39498362+elenilsongomes@users.noreply.github.com>
2022-12-30 13:41:48 -0300

    Add files via upload

---
+++ Questao5/Application/Handlers/ICreateMovimentoResponse.cs
@@ -0,0 +1,9 @@
+﻿using Questao5.Domain.Entities;
+
+namespace Questao5.Application.Handlers
+{
+    public interface ICreateMovimentoResponse
+    {
+        Task<IEnumerable<Movimento>> Get(string idcontacorrente);
+    }
+}
